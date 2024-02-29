4a2e50265fc73ac8767ddf10b09a1c4ca57be48d
Kanailal Manna <kanailalmanna2003@gmail.com>
2024-02-29 22:34:11 +0530

    minor update ⚙️

--- server/src/controllers/order.controller.ts
+++ server/src/controllers/order.controller.ts
@@ -1,18 +1,12 @@
-import { Request, Response, NextFunction } from "express";
-import mongoose from "mongoose";
+import { Request, Response } from "express";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
-import { NewOrderRequestBody } from "../types/types.js";
 import { Order } from "../models/order.model.js";
-import NodeCache from "node-cache";
-import { invalidateCache, reduceStock } from "../utils/features.js"
 import { User } from "../models/user.model.js";
+import { invalidateCache, reduceStock } from "../utils/features.js";
 import { nodeCache } from "../app.js";
+import { NewOrderRequestBody } from "../types/types.js";
 
-
-
-// create a new order
 export const newOrder = asyncHandler(async (
     req: Request<{}, {}, NewOrderRequestBody>,
     res: Response
@@ -26,89 +20,88 @@ export const newOrder = asyncHandler(async (
         shippingCharges,
         discount,
         total
-    } = req.body
+    } = req.body;
 
-    if (!(shippingInfo || orderItems || user || subtotal || tax || total)) {
-        throw new ApiError(400, "All fields are required")
+    if (!(shippingInfo && orderItems && user && subtotal && tax && total)) {
+        throw new ApiError(400, "All fields are required");
     }
 
-    const dbUser = await User.findOne({ _id: user })
+    const dbUser = await User.findOne({ _id: user });
+
+    if (!dbUser) {
+        throw new ApiError(404, "User not found");
+    }
 
     const order = await Order.create({
         shippingInfo,
         orderItems,
-        user: dbUser?._id,
+        user: dbUser._id,
         subtotal,
         tax,
         shippingCharges,
         discount,
         total
-    })
+    });
 
-    await reduceStock(orderItems)
+    await reduceStock(orderItems);
 
     invalidateCache({
         product: true,
         order: true,
         admin: true,
         userId: user,
-        productId: order.orderItems.map((i) => String(i.productId)),
-    })
+        orderId: order._id.toString(),
+    });
 
     return res.status(200).json({
         success: true,
         order
-    })
-})
+    });
+});
 
-// get my all orders
 export const myOrders = asyncHandler(async (req, res) => {
-    const { id: user } = req.query
+    const { id: user } = req.query;
 
-    const key = `my-orders-${user}`
+    const key = `my-orders-${user}`;
     let orders = [];
 
     if (nodeCache.has(key)) {
-        orders = JSON.parse(nodeCache.get(key) as string)
+        orders = JSON.parse(nodeCache.get(key) as string);
     } else {
-        orders = await Order.find({ user })
-        nodeCache.set(key, JSON.stringify(orders))
+        orders = await Order.find({ user });
+        nodeCache.set(key, JSON.stringify(orders));
     }
 
     return res.status(200).json({
         success: true,
         orders,
     });
-})
+});
 
-// get all orders --admin
 export const allOrders = asyncHandler(async (req, res) => {
-
-    const key = `all-orders`
+    const key = `all-orders`;
     let orders = [];
 
     if (nodeCache.has(key)) {
-        orders = JSON.parse(nodeCache.get(key) as string)
+        orders = JSON.parse(nodeCache.get(key) as string);
     } else {
-        orders = await Order.find({})
-        nodeCache.set(key, JSON.stringify(orders))
+        orders = await Order.find({});
+        nodeCache.set(key, JSON.stringify(orders));
     }
 
     return res.status(200).json({
         success: true,
         orders
-    })
-})
+    });
+});
 
-
-// Process order
 export const processOrder = asyncHandler(async (req, res) => {
-    const { id } = req.params
+    const { id } = req.params;
 
-    const order = await Order.findById(id)
+    const order = await Order.findById(id);
 
     if (!order) {
-        throw new ApiError(404, "Order nor found!")
+        throw new ApiError(404, "Order not found!");
     }
 
     switch (order.status) {
@@ -123,60 +116,57 @@ export const processOrder = asyncHandler(async (req, res) => {
             break;
     }
 
-    await order.save()
+    await order.save();
 
     invalidateCache({
         product: false,
         order: true,
         admin: true,
         userId: order.user,
-        orderId: String(order._id)
-    })
+        orderId: order._id.toString()
+    });
 
     return res.status(200).json({
         success: true,
         message: `Order ${order.status} Successfully!`
-    })
-})
+    });
+});
 
-
-// delete order
 export const deleteOrder = asyncHandler(async (req, res) => {
-    const { id } = req.params
-    const order = await Order.findById(id)
+    const { id } = req.params;
+    const order = await Order.findById(id);
     if (!order) {
-        throw new ApiError(404, "Order not found!")
+        throw new ApiError(404, "Order not found!");
     }
 
-    await order.deleteOne()
+    await order.deleteOne();
 
     invalidateCache({
         product: false,
         order: true,
         admin: true,
         userId: order.user,
-        orderId: String(order._id),
+        orderId: order._id.toString(),
     });
 
     return res.status(200).json({
         success: true,
         message: "Order Deleted Successfully",
     });
-})
-
+});
 
 export const getSingleOrder = asyncHandler(async (req, res) => {
-    const { id } = req.params
-    const key = `order-${id}`
+    const { id } = req.params;
+    const key = `order-${id}`;
 
     let order;
 
     if (nodeCache.has(key)) {
-        order = JSON.parse(nodeCache.get(key) as string)
+        order = JSON.parse(nodeCache.get(key) as string);
     } else {
-        order = await Order.findById(id).populate("user", "name")
+        order = await Order.findById(id).populate("user", "name");
         if (!order) {
-            throw new ApiError(404, "Order not found!")
+            throw new ApiError(404, "Order not found!");
         }
         nodeCache.set(key, JSON.stringify(order));
     }
@@ -184,5 +174,5 @@ export const getSingleOrder = asyncHandler(async (req, res) => {
     return res.status(200).json({
         success: true,
         order
-    })
-})
+    });
+});
